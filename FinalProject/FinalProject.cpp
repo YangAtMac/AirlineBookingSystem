0ccaf31ef7f4cdd8dd4a6df1e197bb90a9190044
Eric Gray <graye5@mcmaster.ca>
2018-04-08 20:27:21 -0400

    move curl to class

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,9 +1,8 @@
 #include "stdafx.h"
 #include <ios>
-#include <iostream>
+#include <iostream>	
 #include "sqlite3.h"
 #include <string>
-#include <curl.h>
 
 #include "FinalProject.h"
 #include "Flight.h"
@@ -27,23 +26,6 @@ bool login(User &user);
 void initDB();
 
 Database db;
-
-struct FtpFile {
-  const char *filename;
-  FILE *stream;
-};
- 
-static size_t my_fwrite(void *buffer, size_t size, size_t nmemb,
-                        void *stream)
-{
-  struct FtpFile *out = (struct FtpFile *)stream;
-  if(out && !out->stream) {
-    out->stream = fopen(out->filename, "wb"); 
-    if(!out->stream)
-      return -1; 
-  }
-  return fwrite(buffer, size, nmemb, out->stream);
-}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -80,38 +62,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			case 9:
 				exit(0);
 		}
-		CURL *curl;
-  CURLcode res;
-  struct FtpFile ftpfile = {
-    "yourfile.bin", /* name to store the file as if successful */ 
-    NULL
-  };
- 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
- 
-  curl = curl_easy_init();
-  if(curl) {
-   
-    curl_easy_setopt(curl, CURLOPT_URL,"http://example.com/");
-    
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ftpfile);
-    curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
- 
-    res = curl_easy_perform(curl);
- 
-    curl_easy_cleanup(curl);
- 
-    if(CURLE_OK != res) {
-     
-      fprintf(stderr, "curl told us %d\n", res);
-    }
-  }
-  if(ftpfile.stream)
-    fclose(ftpfile.stream);
- 
-  curl_global_cleanup();
+		
 		system("PAUSE");
 	}
 	cout << "Please press any key to exit the program ..." << endl;
